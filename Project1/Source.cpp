7f6defae8be1ebfac7435c0961d12db4a22bce84
s0nin0 <66138281+s0nin0@users.noreply.github.com>
2020-06-21 17:02:52 +0300

    instruction

---
+++ Project1/Source.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "There will be my arts making in PS.There aren't all my works. There will be another a bit sooner.Enjoy!";
+	system("pause");
+}
